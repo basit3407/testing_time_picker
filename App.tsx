4243a6187aee2c2377d16dca0616c634bf5f51e4
basit3407 <basit3407@gmail.com>
2022-06-17 09:55:06 +0500

    ready for testing

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import * as Updates from "expo-updates";
+import { Button, View, Alert } from "react-native";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
-export default function App() {
+const Example = () => {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  //For force updating when new new update is published via expo-updates. (for production only)
+  useEffect(() => {
+    (async () => {
+      try {
+        const update = await Updates.checkForUpdateAsync();
+        if (update.isAvailable) {
+          await Updates.fetchUpdateAsync();
+          Alert.alert(
+            "Update available",
+            "A new update is available. Restart the app to apply the update.",
+            [{ text: "OK", onPress: async () => await Updates.reloadAsync() }],
+            { cancelable: false }
+          );
+        }
+      } catch (e) {
+        console.warn(e);
+      }
+    })();
+  }, []);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date: Date) => {
+    console.warn("A date has been picked: ", date);
+    hideDatePicker();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Button title="Show Date Picker" onPress={showDatePicker} />
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Example;
