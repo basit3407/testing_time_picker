397147ebf1a2b1351ba0f4f7a02a7754c0535026
basit3407 <basit3407@gmail.com>
2022-06-17 11:20:29 +0500

    works in original library

--- App.tsx
+++ App.tsx
@@ -1,11 +1,32 @@
-import { useState } from "react";
-import { Button, View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { Button, View, Text, Alert } from "react-native";
+import * as Updates from "expo-updates";
 import DateTimePicker from "@react-native-community/datetimepicker";
 
 export const App = () => {
   const [date, setDate] = useState(new Date(1598051730000));
   const [mode, setMode] = useState("date");
   const [show, setShow] = useState(false);
+
+  //For force updating when new new update is published via expo-updates. (for production only)
+  useEffect(() => {
+    (async () => {
+      try {
+        const update = await Updates.checkForUpdateAsync();
+        if (update.isAvailable) {
+          await Updates.fetchUpdateAsync();
+          Alert.alert(
+            "Update available",
+            "A new update is available. Restart the app to apply the update.",
+            [{ text: "OK", onPress: async () => await Updates.reloadAsync() }],
+            { cancelable: false }
+          );
+        }
+      } catch (e) {
+        console.warn(e);
+      }
+    })();
+  }, []);
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate;
@@ -47,3 +68,5 @@ export const App = () => {
     </View>
   );
 };
+
+export default App;
