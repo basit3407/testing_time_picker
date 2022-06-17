015e554606b22317550a9c98e3bad5db311cf81d
basit3407 <basit3407@gmail.com>
2022-06-17 10:58:29 +0500

    changed library

--- App.tsx
+++ App.tsx
@@ -1,56 +1,49 @@
-import { useEffect, useState } from "react";
-import * as Updates from "expo-updates";
-import { Button, View, Alert } from "react-native";
-import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { useState } from "react";
+import { Button, View, Text } from "react-native";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
-const Example = () => {
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+export const App = () => {
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
 
-  //For force updating when new new update is published via expo-updates. (for production only)
-  useEffect(() => {
-    (async () => {
-      try {
-        const update = await Updates.checkForUpdateAsync();
-        if (update.isAvailable) {
-          await Updates.fetchUpdateAsync();
-          Alert.alert(
-            "Update available",
-            "A new update is available. Restart the app to apply the update.",
-            [{ text: "OK", onPress: async () => await Updates.reloadAsync() }],
-            { cancelable: false }
-          );
-        }
-      } catch (e) {
-        console.warn(e);
-      }
-    })();
-  }, []);
-
-  const showDatePicker = () => {
-    setDatePickerVisibility(true);
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
   };
 
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false);
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
   };
 
-  const handleConfirm = (date: Date) => {
-    console.warn("A date has been picked: ", date);
-    hideDatePicker();
+  const showDatepicker = () => {
+    showMode("date");
+  };
+
+  const showTimepicker = () => {
+    showMode("time");
   };
 
   return (
     <View>
-      <Button title="Show Date Picker" onPress={showDatePicker} />
-      <DateTimePickerModal
-        isVisible={isDatePickerVisible}
-        maximumDate={new Date()}
-        mode="time"
-        onConfirm={handleConfirm}
-        onCancel={hideDatePicker}
-      />
+      <View>
+        <Button onPress={showDatepicker} title="Show date picker!" />
+      </View>
+      <View>
+        <Button onPress={showTimepicker} title="Show time picker!" />
+      </View>
+      <Text>selected: {date.toLocaleString()}</Text>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode={mode}
+          is24Hour={true}
+          onChange={onChange}
+        />
+      )}
     </View>
   );
 };
-
-export default Example;
